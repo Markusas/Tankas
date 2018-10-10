4998bd599f14ef5942aad6765c2437b3162edc0c
marek <saltinio@gmail.com>
2018-10-10 20:49:35 +0300

    Implementation of class Tankas

--- src/main/java/org/martynas/model/Tankas.java
+++ src/main/java/org/martynas/model/Tankas.java
@@ -1,4 +1,29 @@
 package org.martynas.model;
 
+import java.awt.geom.Point2D;
+
 public class Tankas {
+    private Point2D koordinates;
+
+    private String kryptis;
+
+    private
+
+    public void pirmyn() {
+    }
+
+    public void desinen() {
+    }
+
+    public void atgal() {
+    }
+
+    public void kairen() {
+    }
+
+    public void suvis() {
+    }
+
+    public void info() {
+    }
 }
--- src/main/java/org/martynas/tankasApp/AppTankas.java
+++ src/main/java/org/martynas/tankasApp/AppTankas.java
@@ -1,7 +1,22 @@
 package org.martynas.tankasApp;
 
+import org.martynas.model.Tankas;
+
+import java.util.Scanner;
+
 public class AppTankas {
     public static void main(String[] args) {
-
+        Tankas tankas = new Tankas();
+        Scanner sc = new Scanner(System.in);
+        boolean runProgram = true;
+        while(runProgram){
+            tankas.pirmyn();
+            tankas.desinen();
+            tankas.atgal();
+            tankas.kairen();
+            tankas.suvis();
+            tankas.info();
+        }
+        sc.close();
     }
 }
